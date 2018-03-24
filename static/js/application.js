0d72f490608c3a90c7e52bef6fce3dba0be7d16e
dotzero <mail@dotzero.ru>
2018-03-24 09:00:29 +0300

    Remove unused js

--- static/js/application.js
+++ static/js/application.js
@@ -1,24 +1,21 @@
 $(function() {
-    var $textarea = $("#contents"),
-        $printarea = $('#printable_contents'),
-        $loading = $("#loading"),
-        content = $textarea.val();
+  var $textarea = $("#contents"),
+      $printarea = $('#printable_contents'),
+      $loading = $("#loading"),
+      content = $textarea.val();
 
-    $textarea.tabby();
-    $textarea.focus();
+  $printarea.text(content);
 
-    $printarea.text(content);
+  setInterval(function() {
+    if (content !== $textarea.val()) {
+      content = $textarea.val();
 
-    setInterval(function() {
-      if (content !== $textarea.val()) {
-        content = $textarea.val();
+      $loading.show();
+      $.post('', { t: content }).always(function() {
+        $loading.hide();
+      });
 
-        $loading.show();
-        $.post('', { t: content }).always(function() {
-          $loading.hide();
-        });
-
-        $printarea.text(content);
-      }
-    }, 1000);
+      $printarea.text(content);
+    }
+  }, 1000);
 });
